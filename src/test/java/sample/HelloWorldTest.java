3aa742e18581a2a2dfa1de620fa778be04f01da2
tenten0213 <takehito.0213@gmail.com>
2014-09-17 05:55:36 +0900

    move set RestAssured.port to setUp method

--- src/test/java/sample/HelloWorldTest.java
+++ src/test/java/sample/HelloWorldTest.java
@@ -2,24 +2,23 @@ package sample;
 
 import com.jayway.restassured.RestAssured;
 import org.junit.Test;
-
+import org.junit.BeforeClass;
 import static com.jayway.restassured.RestAssured.get;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.Is.is;
-import org.junit.BeforeClass;
 
 
 public class HelloWorldTest {
 
     @BeforeClass
     public static void setUp() {
+        RestAssured.port = 4567;
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.init();
     }
 
     @Test
     public void testHello() throws Exception {
-        RestAssured.port = 4567;
         String body = get("/hello").andReturn().body().asString();
         assertThat(body, is("Hello World!"));
     }
